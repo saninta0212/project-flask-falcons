ff6a4c95fcf275622706d828a57b0682a68f8ebd
oscar-me <oscar-me@outlook.com>
2022-06-03 14:09:41 -0500

    add work experience info

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,27 @@ def index():
 
 @app.route('/experience')
 def experience():
-    return render_template('work_experience.html', title="Work experience", url=os.getenv("URL"))
+    def format_job(title, date, organization, logo_url):
+        return {
+            "title": title,
+            "date": date,
+            "organization": organization,
+            "logo_url": logo_url
+        }
+
+    job_list = [
+        format_job("Undergrad Co-op - R&D (Data Informatics/Molecular Biology)", "May 2021 - December 2021", "Amgen",
+                   "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.jobsexpo.ie%2Fwp-content%2Fuploads%2F2018%2F09%2FAmgen-Logo-PNG-Transparent-800.png&f=1&nofb=1"),
+        format_job("Bioinformatics Technology Lab Co-op with Dr Inanc Birol", "September 2020 - April 2021", "BC Cancer Genome Sciences Centre",
+                   "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F2319317%3Fs%3D200%26v%3D4&f=1&nofb=1"),
+        format_job("Undergraduate Research Assistant at Dr. Jamie Zeitzer’s lab", "May 2021- Present",
+                   "Wu Tsai Neurosciences Institute at Stanford University", "https://neuroscience.stanford.edu/sites/g/files/sbiybj1576/f/neuro-signature-logo.png"),
+        format_job("AMS Tutoring Supervisor", "September 2020 - Present",
+                   "Alma Mater Society UBC", "https://duckduckgo.com/i/db0508c6.png"),
+        format_job("AMS Tutor", "September 2019 - August 2020",
+                   "Alma Mater Society UBC", "https://duckduckgo.com/i/db0508c6.png")
+    ]
+    return render_template('work_experience.html', title="Work experience", url=os.getenv("URL"), job_list=job_list)
 
 
 @app.route('/hobbies')
