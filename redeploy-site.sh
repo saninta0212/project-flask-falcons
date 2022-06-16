2344273ca5704f5bdd3f80865657e556f40d0132
Sambina Islam Aninta <50872594+saninta0212@users.noreply.github.com>
2022-06-16 11:58:35 -0700

    fixed a bug to run in detached tmux session

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -5,9 +5,5 @@ cd project-flask-falcons
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux -d new -s $1
 cd app
-export FLASK_ENV=development
-flask run --host=0.0.0.0
-
-
+tmux new -d -s $1 flask run --host=0.0.0.0
