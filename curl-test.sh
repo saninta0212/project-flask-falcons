7d2de7a9f039f6453966971ef388166fcdc9bd88
Sambina Islam Aninta <sambina.islam@gmail.com>
2022-06-27 18:40:12 -0700

    added endpoints

--- curl-test.sh
+++ curl-test.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-curl --request GET http://localhost:5000/api/timeline_post
-curl http://localhost:5000/api/timeline_post 
-
-
-curl --request POST http://localhost:5000/api/timeline_post -d 'name=Sam&email=sambina@gmail.com&content=Checking endpoints'
   
 curl -X  POST http://localhost:5000/api/timeline_post -d 'name=Sam&email=sambina@gmail.com&content=Checking endpoints'  
 
