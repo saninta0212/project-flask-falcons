e4783e29c064b8395e80f3341d21ddce604166d0
Sambina Islam Aninta <sambina.islam@gmail.com>
2022-07-05 00:33:44 -0700

    changed redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,6 +3,5 @@
 
 cd project-flask-falcons
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
